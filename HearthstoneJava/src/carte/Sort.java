512ab85223b3f36e3d9ed6d1a28c264f9da68cb6
Julian <drakox211@gmail.com>
2018-05-18 11:02:39 +0200

    Update Sort.java

--- HearthstoneJava/src/carte/Sort.java
+++ HearthstoneJava/src/carte/Sort.java
@@ -3,10 +3,26 @@ package carte;
 import jeu.IJoueur;
 
 public class Sort extends Carte {
+	
+	private Capacite capacite;
 
-	public Sort(String n, int c, IJoueur p) {
+	public Sort(String n, int c, IJoueur p, Capacite capacite) {
 		super(n, c, p);
+		this.setCapacite(capacite);
+		
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void setCapacite(Capacite c) {
+		this.capacite=c;
+	}
+	
+	public Capacite getCapacite() {
+		return this.capacite;
+	}
+	
+	public String toString() {
+		return super.toString() + (this.getCapacite()).toString();
 	}
 
 }
